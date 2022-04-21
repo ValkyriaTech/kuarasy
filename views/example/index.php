93e1c719ac91817f50db6b99f96809f5776921c5
Victor Andeloci <victor.andeloci@gmail.com>
2022-04-20 23:39:29 -0300

    update example view

--- views/example/index.php
+++ views/example/index.php
@@ -3,9 +3,16 @@
   <head>
     <meta charset="utf-8">
     <title>It works!</title>
+
+    <style media="screen">
+      code {
+        color: #b00;
+      }
+    </style>
   </head>
   <body>
     <h1>Hello, world!</h1>
+    <p>You are seeing a view. It's located in <code>/views/example</code>. Thiw view is setted as default in <code>/config.php</code></p>
     <a href="//<?= $_SERVER['SERVER_NAME'] . $_SERVER['REQUEST_URI'] ?>example/test">Access the subview</a>
   </body>
 </html>
